be09232ca1d4d1aca6300317887a1e3d26efbaf3
ston1x <stoianovnk@gmail.com>
2018-05-27 00:07:04 +0300

    Uncomment api line for heroku

--- config/initializers/saltedge.rb
+++ config/initializers/saltedge.rb
@@ -1,2 +1,2 @@
 # Add APP_ID, SECRET and PRIVATE_PEM_PATH as your environment variables
-#api = Saltedge.new(ENV['APP_ID'], ENV['SECRET'], ENV['PRIVATE_PEM_PATH'])
+api = Saltedge.new(ENV['APP_ID'], ENV['SECRET'], ENV['PRIVATE_PEM_PATH'])
