0dfe5e9deae95be9cacadc5146ffde16c9da6c59
ston1x <stoianovnk@gmail.com>
2018-06-26 23:01:14 +0300

    Create service object for fetch_accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,8 +2,9 @@ require 'tasks/account_tasks'
 class AccountsController < ApplicationController
   def fetch
     login = Login.find_by(login_id: params[:login_id])
-    accounts = Tasks::AccountTasks.new.fetch(params[:login_id])
     
+    fetch_accounts_service = FetchAccounts.new
+    fetch_accounts_service.perform(params[:login_id])
     redirect_to login
   end
 
---
+++ app/services/fetch_accounts.rb
@@ -0,0 +1,29 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+class FetchAccounts
+  def perform(login_id)
+    login = Login.find_by(login_id: login_id)
+    # Destroy all existing accounts in the database to refresh them
+    login.accounts.destroy_all if login.accounts.any?
+    response = API.request(:get, 'https://www.saltedge.com/api/v4/accounts', {'data' => {'login_id' => login_id}})
+    accounts = JSON.parse response.body
+    accounts = accounts['data']
+    accounts.each do |account|
+      a = Account.new
+      a.account_id = account['id']
+      a.login_id = login.id
+      a.name = account['name']
+      a.balance = account['balance']
+      a.currency_code = account['currency_code']
+      a.nature = account['nature']
+      transactions_posted = account['extra']['transactions_count']['posted'].to_i
+      transactions_pending = account['extra']['transactions_count']['pending'].to_i
+      a.transactions_count = transactions_posted + transactions_pending
+      puts 'Saving account'
+      a.save
+      Tasks::TransactionTasks.new.fetch(a.id)
+    end
+  end
+end
