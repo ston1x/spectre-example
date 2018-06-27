9f00b71ecff21ee4c95897bad34527567452d186
ston1x <stoianovnk@gmail.com>
2018-06-28 02:11:23 +0300

    Memoize service object logins, render API errors

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,19 +1,13 @@
 require 'tasks/login_tasks'
 
 class LoginsController < ApplicationController
-  @@connect_login_service = ConnectLogin.new
-  @@save_login_service = SaveLogin.new
-  @@reconnect_login_service = ReconnectLogin.new
-  @@refresh_login_service = RefreshLogin.new
-  @@remove_login_service = RemoveLogin.new
-
-  def create_login  
-    connect_url = @@connect_login_service.perform(current_user.id)
+  def create_login
+    connect_url = connect_login_service.perform(current_user.id)
     redirect_to connect_url
   end
 
   def save_login
-    @@save_login_service.perform(current_user.id)
+    save_login_service.perform(current_user.id)
   end
 
   def callback_success
@@ -40,21 +34,53 @@ class LoginsController < ApplicationController
       'login' => params[:login],
       'password' => params[:password]
     }
-    response = @@reconnect_login_service.perform(params[:login_id], credentials)
-    render json: response
+    response = reconnect_login_service.perform(params[:login_id], credentials)
+    if response['error_message']
+      flash[:alert] = response['error_message']
+    else
+      flash[:notice] = 'Login successfully reconnected'
+    end
+    redirect_to logins_path
   end
 
   def login_credentials
   end
 
   def refresh_login
-    response = @@refresh_login_service.perform(params[:login_id])
-    render json: response
+    response = refresh_login_service.perform(params[:login_id])
+    next_refresh = response['next_refresh_possible_at']
+    if response['error_message']
+      flash[:alert] = "#{response['error_message']} Next refresh possible at: #{next_refresh}"
+    else
+      flash[:notice] = 'Login successfully refreshed'
+    end
+    redirect_to logins_path
   end
 
   def remove_login
-    @@remove_login_service.perform(params[:login_id])
+    remove_login_service.perform(params[:login_id])
     Login.find_by(login_id: params[:login_id]).destroy
     redirect_to logins_path
+  end
+
+  # Memoize service objects
+  def connect_login_service
+    @connect_login_service ||= ConnectLogin.new
+  end
+
+  def save_login_service
+    @save_login_service ||= SaveLogin.new
+  end
+
+  def reconnect_login_service
+    @reconnect_login_service ||= ReconnectLogin.new
+  end
+
+  def refresh_login_service
+    @refresh_login_service ||= RefreshLogin.new
+  end
+
+  def remove_login_service
+    @remove_login_service ||= RemoveLogin.new
   end
 end
